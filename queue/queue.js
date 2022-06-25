c85e91f26f94d3ccb547185ae28db12603266a94
w1ld0uth <shurygin1vs@gmail.com>
2022-06-25 16:04:49 +0300

    Created queue

--- queue/queue.js
+++ queue/queue.js
@@ -1,3 +1,41 @@
-class Queue {
+export class Queue {
+    constructor() {
+        this.elements = {};
+        this.head = 0;
+        this.tail = 0;
+    }
 
+    enqueue(element) {
+        this.elements[this.tail] = element;
+        this.tail++;
+    }
+
+    dequeue() {
+        delete this.elements[this.head];
+        this.head++;
+    }
+
+    peak() {
+        return this.elements[this.head];
+    }
+
+    get length() {
+        return this.tail - this.head;
+    }
+
+    get isEmpty() {
+        return this.length === 0;
+    }
 }
+
+// Tests
+
+// let queue = new Queue();
+// queue.enqueue(1);
+// queue.enqueue(2);
+// queue.enqueue(3);
+// console.log(queue.peak());
+// queue.dequeue();
+// console.log(queue.peak());
+// queue.enqueue(4);
+// console.log(queue.peak());
