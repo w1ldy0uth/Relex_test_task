1778769685e8fc715f0f43dfbf9fc31960bb4348
w1ld0uth <shurygin1vs@gmail.com>
2022-06-26 22:48:29 +0300

    Finished Human and Computer classes

---
+++ player/computer.js
@@ -0,0 +1,46 @@
+import {Player, findMinCard} from "./player.js";
+
+export class Computer extends Player {
+    attack(human) {
+        let battleResults = {
+            toAttacker: [],
+            toDefender: -1
+        };
+
+        console.log("Computer's defence cards: ", human.defence.join(" "));
+
+        let i = 0;
+        while (i < 3) {
+            let currentAttackCard = this.cards.peak();
+            this.cards.dequeue();
+            console.log("Computer's attacking card is ", currentAttackCard);
+
+            let minCard = findMinCard(human.defence);
+            if (currentAttackCard < minCard) {
+                battleResults.toDefender = currentAttackCard;
+                break;
+            }
+            else if (currentAttackCard === minCard) {
+                this.cards.enqueue(currentAttackCard);
+            }
+            else {
+                human.defence[human.defence.indexOf(minCard, 0)] = -1;
+                battleResults.toAttacker.push(minCard);
+                i++;
+            }
+        }
+
+        if (human.defence.every(item => item === -1)) {
+            human.penalties++;
+        }
+
+        if (battleResults.toDefender !== -1) {
+            human.cards.enqueue(battleResults.toDefender);
+        }
+        for (let i = 0; i < battleResults.toAttacker.length; i++) {
+            if (battleResults.toAttacker[i] !== -1) {
+                this.cards.enqueue(battleResults.toAttacker[i]);
+            }
+        }
+    }
+}
---
+++ player/human.js
@@ -0,0 +1,51 @@
+import {findMinCard, Player} from "./player.js";
+
+const readline = require("prompt-sync")();
+
+export class Human extends Player {
+    attack(computer) {
+        let battleResults = {
+            toAttacker: [],
+            toDefender: -1
+        };
+
+        console.log("Computer's defence cards: ", computer.defence.join(" "));
+
+        let i = 0;
+        while (i < 3) {
+            let currentAttackCard = this.cards.peak();
+            this.cards.dequeue();
+            console.log("Your attacking card is ", currentAttackCard);
+
+            if (currentAttackCard < findMinCard(computer.defence)) {
+                console.log(currentAttackCard, " isn't strong enough to beat computer's defence, end of stroke.");
+                battleResults.toDefender = currentAttackCard;
+                break;
+            }
+            else {
+                const cardToBeat = readline("Choose a computer's card to beat: ");
+                if (cardToBeat === currentAttackCard) {
+                    console.log("Card are equal, choosing next from queue");
+                }
+                else {
+                    computer.defence[computer.defence.indexOf(cardToBeat, 0)] = -1;
+                    battleResults.toAttacker.push(cardToBeat);
+                    i++;
+                }
+            }
+        }
+
+        if (computer.defence.every(item => item === -1)) {
+            computer.penalties++;
+        }
+
+        if (battleResults.toDefender !== -1) {
+            computer.cards.enqueue(battleResults.toDefender);
+        }
+        for (let i = 0; i < battleResults.toAttacker.length; i++) {
+            if (battleResults.toAttacker[i] !== -1) {
+                this.cards.enqueue(battleResults.toAttacker[i]);
+            }
+        }
+    }
+}
