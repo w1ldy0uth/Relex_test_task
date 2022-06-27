b50e0f77fe08d9947f968edf0cca80f3d2c3b5c5
w1ld0uth <shurygin1vs@gmail.com>
2022-06-27 23:18:56 +0300

    Fixed human behaviour

--- player/human.js
+++ player/human.js
@@ -9,44 +9,58 @@ class Human extends mod.Player {
             toDefender: -1
         };
 
-        console.log("Computer's defence cards: ", computer.defence.join(" "));
-
         let i = 0;
         while (i < 3) {
-            let currentAttackCard = this.cards.peak();
-            this.cards.dequeue();
-            console.log("Your attacking card is ", currentAttackCard);
+            console.log("Computer's defence cards:", computer.defence.join(" "));
 
-            if (currentAttackCard < mod.findMinCard(computer.defence)) {
-                console.log(currentAttackCard, " isn't strong enough to beat computer's defence, end of stroke.");
+            let currentAttackCard = this.cards.peak();
+            console.log("Your attacking card is", currentAttackCard);
+
+            let cardToBeat = readline("Choose a computer's card to beat: ");
+            cardToBeat = parseInt(cardToBeat);
+
+            if (cardToBeat === -1 || computer.defence.indexOf(cardToBeat, 0) === -1) {
+                console.log("Wrong card picked, try one again");
+            }
+            else if (currentAttackCard < cardToBeat && !(currentAttackCard === 0 && cardToBeat === 4)) {
+                console.log(currentAttackCard, "isn't strong enough to beat computer's defence");
                 battleResults.toDefender = currentAttackCard;
+                this.cards.dequeue();
                 break;
             }
-            else {
-                const cardToBeat = readline("Choose a computer's card to beat: ");
-                if (cardToBeat === currentAttackCard) {
-                    console.log("Card are equal, choosing next from queue");
-                }
-                else {
-                    computer.defence[computer.defence.indexOf(cardToBeat, 0)] = -1;
-                    battleResults.toAttacker.push(cardToBeat);
-                    i++;
-                }
+            else if (cardToBeat === currentAttackCard) {
+                console.log("Card are equal, choosing next from queue");
+                this.cards.dequeue();
+                this.cards.enqueue(currentAttackCard);
+            }
+            else if (currentAttackCard > cardToBeat || (cardToBeat === 4 && currentAttackCard === 0)) {
+                computer.defence[computer.defence.indexOf(cardToBeat, 0)] = -1;
+                battleResults.toAttacker.push(cardToBeat);
+                this.cards.dequeue();
+                i++;
             }
         }
 
+        console.log("\nEnd of turn");
         if (computer.defence.every(item => item === -1)) {
             computer.penalties++;
+            console.log("Computer got plus one penalty score. Its current penalties:", computer.penalties);
         }
 
         if (battleResults.toDefender !== -1) {
+            console.log("Computer got new card:", battleResults.toDefender);
             computer.cards.enqueue(battleResults.toDefender);
         }
-        for (let i = 0; i < battleResults.toAttacker.length; i++) {
-            if (battleResults.toAttacker[i] !== -1) {
-                this.cards.enqueue(battleResults.toAttacker[i]);
-            }
+        if (battleResults.toAttacker.length === 0) {
+            console.log("You haven't beat any card of computer");
         }
+        else {
+            console.log("You beat this computer's cards:", battleResults.toAttacker.join(" "));
+        }
+        for (let i = 0; i < battleResults.toAttacker.length; i++) {
+            this.cards.enqueue(battleResults.toAttacker[i]);
+        }
+        console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n");
     }
 }
 
